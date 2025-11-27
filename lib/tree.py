347ebb2e4af7f25e6bae4bc7cda4d9fcfc4fab55
akamnovelakwa-ship-it <akam.akwa@student.moringaschool.com>
2025-11-27 22:20:40 +0300

    lab

--- lib/tree.py
+++ lib/tree.py
@@ -1,20 +1,21 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    if self.root is None:
-      return None
+    def get_element_by_id(self, id):
+        def _get_element_by_id(node, id):
+            if node is None:
+                return None
 
-    def dfs(node):
-      if node is None:
-        return None
-      if node.get('id') == id:
-        return node
-      for child in node.get('children', []):
-        found = dfs(child)
-        if found is not None:
-          return found
-      return None
+            # dict-based node access
+            if node.get("id") == id:
+                return node
 
-    return dfs(self.root)
+            for child in node.get("children", []):
+                result = _get_element_by_id(child, id)
+                if result is not None:
+                    return result
+
+            return None
+
+        return _get_element_by_id(self.root, id)
